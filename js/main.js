c2383070783d6387cab63ba176c592bca1fea402
Derryk Boyd <derrykboyd@gmail.com>
2020-07-26 22:52:22 -0600

    Add bottom nav mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-// Typewriter on home page
-const greetings = ['Hi there 🙂', 'Salut!  😎', 'Hello  👋', 'Bonjour  👋'];
+// hide typewriter on animation end
 const typewriter = document.querySelector('.typewriter');
-let greetingNum = 1;
+typewriter.addEventListener('animationend', e => {
+  if (e.animationName === 'typewriter') {
+    setTimeout(() => {
+      e.target.style['border-right-color'] = 'transparent';
+    }, 500);
+  }
+})
 
-setInterval(() => {
-  typewriter.classList.remove('typewriter');
-  void typewriter.offsetWidth;
-  typewriter.classList.add('typewriter');
-  typewriter.innerHTML = greetings[greetingNum % greetings.length];
-  greetingNum ++;
-}, 4000);
+// highlight bottom nav based on scroll
+const btmNavItems = document.querySelectorAll('.btm-nav-item');
+const tech = document.querySelector('#tech');
+const projects = document.querySelector('#projects');
+const education = document.querySelector('#education');
+
+document.addEventListener('scroll', _ => {
+  btmNavItems.forEach(item => item.classList.remove('active'))
+  if (window.scrollY < tech.offsetTop) btmNavItems[0].classList.add('active');
+  else if (window.scrollY < projects.offsetTop) btmNavItems[1].classList.add('active');
+  else if (window.scrollY < education.offsetTop) btmNavItems[2].classList.add('active');
+  else btmNavItems[3].classList.add('active');
+})
